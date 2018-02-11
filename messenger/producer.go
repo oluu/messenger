7fe038e27dd77a1dfd9a5f53ac8f8b7ad4196f4b
willtodd03 <willtodd03@gmail.com>
2018-02-11 13:47:35 -0500

    Added funcs for Producer

--- messenger/producer.go
+++ messenger/producer.go
@@ -2,11 +2,16 @@ package messenger
 
 import "github.com/Shopify/sarama"
 
+// Producer implements sarama.AsyncProducer
+type Producer struct {
+	producer sarama.AsyncProducer
+}
+
 // NewProducer returns a new instance of a Kafka Producer. connectionString is the connection information. topics is an
 // array of kafka topics. successCallback is a function that is performed when a message is successfully published.
 // errorCallback is a function that is performed when a message fails to publish.
-func NewProducer(connectionString string, topics []string, successCallback func(), errorCallback func()) (sarama.AsyncProducer, error) {
-	// initialize configurations for kafka
+func NewProducer(connectionString string, topics []string, successCallback, errorCallback func()) (sarama.AsyncProducer, error) {
+	// initialize configurations for kafka producer
 	config := sarama.NewConfig()
 	if successCallback != nil {
 		config.Producer.Return.Successes = true
@@ -33,4 +38,21 @@ func NewProducer(connectionString string, topics []string, successCallback func(
 		}()
 	}
 	return producer, nil
+}
+
+// Close handles closing and cleaning up the producer. Call this if you're done using the producer
+func (p *Producer) Close() {
+	if p.producer != nil {
+		err := p.producer.Close()
+		if err != nil {
+			// TODO: log error
+		}
+	}
+}
+
+// Publish handles sending the message to the producer queue. message is the value sent and topic is the destination
+// topic the message is sent to.
+func (p *Producer) Publish(message, topic string) {
+	producerMessage := &sarama.ProducerMessage{Topic: topic, Value: sarama.StringEncoder(message)}
+	p.producer.Input() <- producerMessage
 }
